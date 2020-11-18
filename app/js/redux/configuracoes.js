9ef7d317dc86fdcc08eb5c4ef03353fa479c0c80
Bruno <brunohsa@hotmail.com>
2020-11-17 22:52:40 -0300

    Ajustes

--- app/js/redux/configuracoes.js
+++ app/js/redux/configuracoes.js
@@ -1,10 +1,10 @@
 const configuracoes = {
-  URL_MS_AUTENTICACAO: 'http://192.168.0.49:8081/autenticacao/api/',
-  URL_MS_CARDAPIO: 'http://localhost:8083/cardapio/api/',
-  URL_MS_CARRINHO: 'http://localhost:8084/carrinho/api/',
-  URL_MS_CADASTRO: 'http://localhost:8082/cadastro/api/',
-  URL_MS_LOCALIZACAO: 'http://localhost:8086/localizacao/api/',
-  URL_MS_DOWLOAD_IMAGEM_FORNECEDOR: 'http://localhost:8082/cadastro/api/v1/pessoa-juridica/%s/imagem/download',
+  URL_MS_AUTENTICACAO: 'http://appetito.brazilsouth.cloudapp.azure.com:8081/autenticacao/api/',
+  URL_MS_CARDAPIO: 'http://appetito.brazilsouth.cloudapp.azure.com:8083/cardapio/api/',
+  URL_MS_CARRINHO: 'http://appetito.brazilsouth.cloudapp.azure.com:8084/carrinho/api/',
+  URL_MS_CADASTRO: 'http://appetito.brazilsouth.cloudapp.azure.com:8082/cadastro/api/',
+  URL_MS_LOCALIZACAO: 'http://appetito.brazilsouth.cloudapp.azure.com:8086/localizacao/api/',
+  URL_MS_DOWLOAD_IMAGEM_FORNECEDOR: 'http://appetito.brazilsouth.cloudapp.azure.com:8082/cadastro/api/v1/pessoa-juridica/%s/imagem/download',
 }
 
 
